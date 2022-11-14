dec5862407f87b7c6bc208916d727f49d68d72d1
Daniel Aimacaña <daniel.aimacana@epn.edu.ec>
2022-11-14 17:56:24 -0500

    varaibles

---
+++ 01-javascript/01-variables.js
@@ -0,0 +1,28 @@
+//Mutables(reasignadas)
+var numeroUno = 1;
+let numeroDos = 2;
+numeroUno = 12;
+numeroDos = 8;
+numeroUno = true;
+numeroDos = false;
+
+//Inmutables ()
+const configuracionArchivos = 'PDF'
+//Vamos a preferir CONST -> LET > nunca VAR!
+
+//Tipos de variables (primitivas)
+const numero = 1;// number
+const sueldo = 1.2;// number
+const texto = 'Adrian';// String ""
+const apellido= 'Eguez';// string
+const booleano = true;// boolean
+const hijos = null;// object
+const zapatos = undefined;// undefined
+
+console.log(typeof numero);
+console.log(typeof sueldo);
+console.log(typeof texto);
+console.log(typeof apellido);
+console.log(typeof booleano);
+console.log(typeof hijos);
+console.log(typeof zapatos);
