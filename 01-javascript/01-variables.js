00fc9925337e47394f2d5db243e20e1038108eb6
Daniel Aimacaña <daniel.aimacana@epn.edu.ec>
2022-11-17 17:54:04 -0500

    Arreglos y primitivas

--- 01-javascript/01-variables.js
+++ 01-javascript/01-variables.js
@@ -26,3 +26,135 @@ console.log(typeof apellido);
 console.log(typeof booleano);
 console.log(typeof hijos);
 console.log(typeof zapatos);
+
+//Truty Falsy
+if(""){ //Falso
+    console.log("String vacio es verdadero")
+}else{
+    console.log("String vacio es falso")
+}
+
+if("daniel"){ //Verdadero
+    console.log("String lleno es verdadero")
+}else{
+    console.log("String lleno es falso")
+}
+
+if(-1){ //Verdadero
+    console.log("-1 Verdadero")
+}else{
+    console.log("-1 Falso")
+}
+
+if(0){ //Falso
+    console.log("0 Verdadero")
+}else{
+    console.log("0 Falso")
+}
+
+if(1){ //Verdadero
+    console.log("1 Verdadero")
+}else{
+    console.log("1 Falso")
+}
+
+if(null){ //Falso
+    console.log("null Verdadero")
+}else{
+    console.log("null Falso")
+}
+
+if(undefined){ //Falso
+    console.log("Null Verdadero")
+}else{
+    console.log("Undefined Falso")
+}
+
+//Orden ed importancia
+// 1) "const"
+// 2) "let"
+// 3) X -> "var"
+
+const daniel = {
+    "nombre": "Daniel",
+    "apellido": "Aimacaña",
+    edad: 22,
+    hijos: null,
+    zapatos: undefined,
+    casado: false,
+    ropa: {
+        color: "cafe",
+        talla: 38,
+    },
+    mascotas: ["Hela","Bongo"]
+};
+console.log(daniel);
+console.log(daniel.mascotas);
+console.log(daniel["mascotas"]);
+daniel.nombre = "Reno";
+console.log(daniel.nombre);
+daniel.sueldo;
+console.log(daniel.sueldo);
+daniel.sueldo = 500;
+console.log(daniel.sueldo);
+daniel["gastos"] = 200;
+console.log(daniel.gastos);
+console.log(daniel);
+//Borrar el valor de una propiedad
+daniel.apellido = undefined;
+console.log(daniel);
+console.log(Object.keys(daniel));
+console.log(Object.values(daniel));
+
+//Delete la llave y el valor dentro del objeto
+delete daniel.apellido;
+console.log(daniel);
+console.log(Object.keys(daniel));
+
+
+// Variables por valor o referencia
+// Variables por valor en JS son las primitivas: number, string, boolean
+let danieEdad = 22;
+let edadJuan = danieEdad; //Guardamos una primitiva en otra variable
+                            //Variables por valor
+
+console.log(danieEdad);//22
+console.log(edadJuan);//22
+danieEdad = +1;
+console.log(danieEdad);//23
+console.log(edadJuan);//22
+
+
+
+// Variable spor Referencia
+let notas = {
+    total: 10
+};
+
+let notasSegundBimestre = notas; //Igualando la referencia
+
+notasSegundBimestre.total = notasSegundBimestre.total + 1;
+console.log(notas);
+console.log(notasSegundBimestre);
+
+// Como clonar objetos
+let notasTercerBimestre = Object.assign({}, notas);
+//Object.assign([], aarreglo)
+
+notasTercerBimestre.total = notasTercerBimestre.total + 1;
+
+console.log('notas', notas);
+console.log('notasSegundoBimestre', notasSegundBimestre);
+console.log('notasTercerBimestre', notasSegundBimestre);
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 01-javascript/02-arreglos.js
@@ -0,0 +1,43 @@
+//02-arreglos.js
+let arreglo = [1,2,3,4,5];
+arreglo = 1;
+arreglo = true;
+arreglo = undefined;
+arreglo = null;
+arreglo = {};
+arreglo = [true, 1, 1.1, 'Adrian', 'Vicente', undefined, null, {}, [1,2]];
+arreglo = [1,2,3,4,5];
+
+//for of
+for (let numero of arreglo){ //Valores
+    console.log('numero', numero);
+}
+
+for (let indice in arreglo){ //Indices
+    arreglo[indice];
+    console.log('indice', indice);
+}
+
+let objetoPrueba = {a:'1', b:'2', c:'3'};
+
+for (let llave in objetoPrueba){
+    console.log('llave', llave);
+}
+
+arreglo.push(11); //Aniadir al final un elemento
+arreglo.pop();
+arreglo.unshift(5);//Aniadir al principio del arreglo
+console.log(arreglo);
+//[5,6,7,8,9,10]
+//splice(indice, numero de elementos eliminados, ... items agregar)
+// EJE  arreglo.splice(0,3,1,2,3, 4, 5, 6)
+arreglo.splice(0,0,4);
+//[4,5,6,7,8,9,10]
+console.log(arreglo)
+
+
+const indiceNueve = arreglo.indexOf(9);  //Encuentra el primer elemento y devuelve el indice
+arreglo.splice(indiceNueve, 2)
+//[4,5,6,7,8]
+console.log(arreglo);
+
